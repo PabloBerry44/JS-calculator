8e7934b343e5629a7dc11669a8f90d0992f54eb6
pabloberry44 <pawel.borowiec@tutanota.com>
2022-06-01 12:26:07 +0200

    basic operations done, iOS style

--- script.js
+++ script.js
@@ -5,86 +5,76 @@ num1 = ''
 num2= ''
 num1saved = false
 num2saved = false
-NaNappeared = false
+let result = 0
+let operators = ['+', '-', '×', '÷']
+let operator = ''
 
-function getNumbers()
-{
-    button.forEach(button => {
-            button.addEventListener('click', ()=>{
-                if(num1saved && num2saved){
-                    console.log(num1, num2, 'done!')
-                }
-                else{
-                    displayContent = display.textContent
-                    if(!isNaN(button.textContent)){
-                        if(!num1saved){
-                            num1 = num1+button.textContent
-                        }
-                        if(num1saved && !num2saved){
-                            num2 = num2+button.textContent
-                        }
-                    }
-                    if(isNaN(button.textContent)){
-                        if(!num1saved){
-                            num1saved=true
-                            num1=parseFloat(num1)
-                        }
-                        if(num1saved && !num2saved){
-                            if(isNaN(button.textContent) && !NaNappeared){
-    
-                            }
-                            else if(isNaN(button.textContent)){
-                                num2saved=true
-                                num2=parseFloat(num2)
-                            }
-                        }
-                        NaNappeared = true
-                    }
-                }
-                display.innerHTML = displayContent+button.textContent
-                console.log('num1=',num1+' ' + 'num2=',num2)
-            })
-    });
+function equation(num1, num2, operator){
+    if(operator=='+'){
+        result=num1+num2
+    }
+    if(operator=='-'){
+        result=num1-num2
+    }
+    if(operator=='×'){
+        result=num1*num2
+    }
+    if(operator=='÷'){
+        result=num1/num2
+    }
+    display.innerHTML = result.toString()
+    num1 = result
+    num2 = ''
+    operator=''
+    num2saved = false
+    getNumbers(num1, num2, num2saved, operator)
 }
 
+function getNumbers(num1, num2, num2saved, operator)
+{
+    button.forEach(button => {
+        button.addEventListener('click', ()=>{
+            display.innerHTML = display.textContent+button.textContent
+            if(button.textContent=='AC'){
+                num1 = ''
+                num2= ''
+                num1saved = false
+                num2saved = false
+                display.innerHTML = ''
+                getNumbers('','',false,'')
+            }
+            // save to both numbers
+            if(!isNaN(button.textContent)){ //if character IS number
+                if(!num1saved){ //if 1st number IS NOT saved:: add characters to it
+                    num1 = num1+button.textContent
+                }
+                if(num1saved && !num2saved){ //if 1st number IS saved but 2nd IS NOT :: add characters to 2nd number
+                    num2 = num2+button.textContent
+                }
+            }
 
-getNumbers()
 
 
+            if(operators.includes(button.textContent) || button.textContent=='='){ //if button is an operator :: save it as operator variable
+                
+                if(operators.includes(button.textContent)){
+                    operator = button.textContent  
+                }
 
+                if(!num1saved){ //if 1st number isn't saved :: save the 1st number
+                    num1saved=true
+                    num1=parseFloat(num1)
+                }
 
-
-
-
-
-
-
-
-
-
-// function getNumbers()
-// {
-//     button.forEach(button => {
-//         button.addEventListener('click', ()=>{
-//             displayContent = display.textContent
-        
-//             if(!isNaN(button.textContent) && num1saved==false){
-//                 num1=num1+button.textContent
-//             }
-//             else if(isNaN(button.textContent)){
-//                 num1 = parseFloat(num1)
-//                 num1saved = true
-//             }
-//             if(!isNaN(button.textContent) && num2saved==false && num1saved==true){
-//                 num2=num2+button.textContent
-//             }
-//             else if(isNaN(button.textContent)){
-//                 num2 = parseFloat(num2)
-//                 num2saved = true
-//             }
-//             display.innerHTML = displayContent+button.textContent
-//             console.log('num1=',num1+' ' + 'num2=',num2)
-//         })
-//     });
-// }
-
+                if(num1saved){ //if 1st number IS saved :: save the 2nd number
+                    if(button.textContent=='='){
+                        num2saved=true
+                        num2=parseFloat(num2)
+                        equation(num1, num2, operator) //pass parameters to the equation function
+                    }
+                }
+            }
+        })
+    });
+}
+getNumbers(num1, num2, num2saved, operator)
